2ea8db74bdc3a5a138766c16a14c77376a4e7587
omar.al.hendi <omar.alhendi2002@gmail.com>
2022-06-09 03:52:59 +0400

    verison 1.0.0

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["source.unsplash.com", "pbs.twimg.com", "swag.vercel.app"],
+  },
+};
